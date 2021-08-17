8f0a97ecff4959a1fa3f63dbfeca8e9af07d7af0
pritheshwar12 <83633767+pritheshwar12@users.noreply.github.com>
2021-08-17 16:55:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+var randomNumber1=Math.floor((Math.random()*6)+1);
+var randomNumber2=Math.floor((Math.random()*6)+1);
+var randomDiceImage="images/"+"dice"+randomNumber1+".png";
+var randomDiceImage2="images/"+"dice"+randomNumber2+".png";
+
+document.querySelectorAll("img")[0].setAttribute("src",randomDiceImage);
+
+document.querySelectorAll("img")[1].setAttribute("src",randomDiceImage2);
+
+if(randomNumber1>randomNumber2){
+document.querySelector("h1").innerHTML="PLAYAER 1 won";
+}
+else if(randomNumber1===randomNumber2)
+{
+  document.querySelector("h1").innerHTML="DRAW";
+}
+else{
+  document.querySelector("h1").innerHTML="PLAYAER 2 won";
+}
